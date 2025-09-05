6917fb349058571394b578570d4b27698a8df6ec
Maitreya05 <maitreyapatil1605@gmail.com>
2025-09-05 13:13:01 +0530

    varibales

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accoutId = 1435
+let accountEmail="maitreya@gmail.com"
+var accountPassword="12345"
+accountCity="Bangalore"
+const accountState="Karnataka"
+console.log(accoutId)
+console.table([accoutId,accountEmail,accountPassword,accountCity,accountState])
