a4794f7dbbe4d19d4e2086795074de273b054fdd
Maitreya05 <maitreyapatil1605@gmail.com>
2025-09-13 21:37:21 +0530

    done

---
+++ 01_basics/06_number_and_maths.js
@@ -0,0 +1,40 @@
+// const score = 400
+// console.log(score)
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+
+// console.log(balance.toFixed(2)); //decimal places
+
+// const otherNumber = 123.8966
+
+// const otherNumber1 = 23.8966
+
+// console.log(otherNumber.toPrecision(3)); //124
+// console.log(otherNumber1.toPrecision(3)); //23.9
+
+// const hunderds = 100000000000
+// const hunderds1= 100000000000
+// console.log(hunderds.toLocaleString());
+
+// console.log(hunderds1.toLocaleString('en-IN'));
+
+// ++++++++++++++++++++Maths++++++++++++++
+
+console.log(Math)
+console.log(Math.abs(-4))
+console.log(Math.round(4.6))
+console.log(Math.ceil(4.2)) //top value-5
+console.log(Math.floor(4.9)) //bottom value-4
+console.log(Math.min(4, 3, 6, 8))
+console.log(Math.max(4, 3, 6, 8))
+
+console.log(Math.randon());  // always btw 0 & 1
+console.log((Math.random() * 10) ); //btw 0 & 10
+console.log((Math.random() * 10) + 1  ); //btw 1 & 10
+
+
+ 
