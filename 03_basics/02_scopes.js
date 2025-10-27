c39494f5417aacc11e8664bda77b18cc28dbd833
Maitreya05 <maitreyapatil1605@gmail.com>
2025-10-27 22:15:25 +0530

    done with mini hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,10 +1,33 @@
-
-if(true){
-    let A = 10
-    const B=20
-    var C = 30
+if (true) {
+  let A = 10;
+  const B = 20;
+  var C = 30;
 }
 // console.log(A)
 // console.log(B)
-console.log(C)
-console.log(A)
+// console.log(C)
+// console.log(A)
+
+function one() {
+  const username = "Maitreya";
+
+  function two() {
+    const website = "youtube";
+    console.log(username);
+    // console.log(website);
+  }
+  
+  two();
+}
+// one();
+if(true){
+    const username ="Maitreya"
+    if(username==="Maitreya"){
+        const website=" youtube"
+        console.log(username+website);
+        
+    }
+    console.log(website);
+}
+console.log(username);
+
